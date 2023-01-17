d5f8b21b3eb5bb036dc8c33a64ff9206378f4056
shaheen <shaheenshoukat047@gmail.com>
2023-01-17 01:52:05 -0800

    first version 1:00hour

--- app/src/main/java/com/example/e_commerce/activities/MainActivity.java
+++ app/src/main/java/com/example/e_commerce/activities/MainActivity.java
@@ -8,8 +8,12 @@ import android.widget.GridLayout;
 
 import com.example.e_commerce.R;
 import com.example.e_commerce.adapters.CategoryAdapter;
+import com.example.e_commerce.adapters.ProductAdapter;
 import com.example.e_commerce.databinding.ActivityMainBinding;
 import com.example.e_commerce.model.Category;
+import com.example.e_commerce.model.Product;
+
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem;
 
 import java.util.ArrayList;
 
@@ -19,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     CategoryAdapter categoryAdapter;
     ArrayList<Category> categories;
 
+    ProductAdapter productAdapter;
+    ArrayList<Product> products;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +34,31 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        initCategories();
+        initProduct();
+        initslider();
+
+    }
+
+    void initslider(){
+        binding.carousel.addData(new CarouselItem("https://tutorials.mianasad.com/ecommerce/uploads/news/special%20offer.jpg","Some Caption"));
+        binding.carousel.addData(new CarouselItem("https://tutorials.mianasad.com/ecommerce/uploads/news/Announcement.jpg","Some Caption"));
+        binding.carousel.addData(new CarouselItem("https://tutorials.mianasad.com/ecommerce/uploads/news/Bridal%20dress.jpg","Some Caption"));
+
+
+    }
+    void initCategories(){
+
         categories = new ArrayList<>();
-        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836493901.png", "#4db151", "some description", 1));
-        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836493901.png", "#685242", "some description", 1));
-        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836493901.png", "#FD0E35", "some description", 1));
-        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836493901.png", "#4db151", "some description", 1));
-        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836493901.png", "#685242", "some description", 1));
-        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836493901.png", "#FD0E35", "some description", 1));
-        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836493901.png", "#4db151", "some description", 1));
-        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836493901.png", "#685242", "some description", 1));
-        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836493901.png", "#FD0E35", "some description", 1));
+        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836546571.png", "#4db151", "some description", 1));
+        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836546571.png", "#685242", "some description", 1));
+        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836546571.png", "#FD0E35", "some description", 1));
+        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836546571.png", "#4db151", "some description", 1));
+        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836546571.png", "#685242", "some description", 1));
+        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836546571.png", "#FD0E35", "some description", 1));
+        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836546571.png", "#4db151", "some description", 1));
+        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836546571.png", "#685242", "some description", 1));
+        categories.add(new Category("Bridal Dress", "https://tutorials.mianasad.com/ecommerce/uploads/category/1673836546571.png", "#FD0E35", "some description", 1));
 
 
         categoryAdapter = new CategoryAdapter(this, categories);
@@ -44,5 +66,24 @@ public class MainActivity extends AppCompatActivity {
         GridLayoutManager layoutManager = new GridLayoutManager(this, 4);
         binding.categoriesList.setLayoutManager(layoutManager);
         binding.categoriesList.setAdapter(categoryAdapter);
+    }
+
+    void initProduct(){
+
+        products =new ArrayList<>();
+        products.add(new Product("Bridal red lehnga","https://tutorials.mianasad.com/ecommerce/uploads/product/1673839815297.jpg","",12,12,1,1));
+        products.add(new Product("Bridal red lehnga","https://tutorials.mianasad.com/ecommerce/uploads/product/1673839815297.jpg","",12,12,1,1));
+        products.add(new Product("Bridal red lehnga","https://tutorials.mianasad.com/ecommerce/uploads/product/1673839815297.jpg","",12,12,1,1));
+        products.add(new Product("Bridal red lehnga","https://tutorials.mianasad.com/ecommerce/uploads/product/1673839815297.jpg","",12,12,1,1));
+        products.add(new Product("Bridal red lehnga","https://tutorials.mianasad.com/ecommerce/uploads/product/1673839815297.jpg","",12,12,1,1));
+        products.add(new Product("Bridal red lehnga","https://tutorials.mianasad.com/ecommerce/uploads/product/1673839815297.jpg","",12,12,1,1));
+        products.add(new Product("Bridal red lehnga","https://tutorials.mianasad.com/ecommerce/uploads/product/1673839815297.jpg","",12,12,1,1));
+        products.add(new Product("Bridal red lehnga","https://tutorials.mianasad.com/ecommerce/uploads/product/1673839815297.jpg","",12,12,1,1));
+        productAdapter=new ProductAdapter(this,products);
+
+        GridLayoutManager layoutManager=new GridLayoutManager(this,2);
+        binding.productlist.setLayoutManager(layoutManager);
+        binding.productlist.setAdapter(productAdapter);
+
     }
 }
